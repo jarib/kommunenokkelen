335c560e52a56a2d453ab38c3f0265747002d504
Jari Bakken <jari.bakken@gmail.com>
2019-04-10 10:45:01 +0200

    do not use front page

--- scraper.rb
+++ scraper.rb
@@ -1,9 +1,7 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
-
+require 'pp'
 require 'nokogiri'
 require 'open-uri'
-require 'scraperwiki'
+# require 'scraperwiki'
 
 def fetch(url)
   p url
@@ -14,29 +12,25 @@ result = []
 baseurl = 'http://www.kommunenokkelen.no'
 
 begin
-  front = fetch(baseurl)
+  front = fetch("#{baseurl}/home")
 
-  front.css('a').select { |e| e.text =~ /^\d\d/ }.map { |e| baseurl + e.attr('href') }.each do |county_url|
-    county = fetch(county_url)
+  front.css('a').select { |e| e.text =~ /^\d+$/ }.map { |e| e.attr('href') }.each do |muni_url|
+    muni = fetch(muni_url)
+    muni_result = {}
 
-    county.css('a').select { |e| e.text.strip =~ /^\d{4}$/ }.map { |e| e.attr('href') }.each do |muni_url|
-      muni = fetch(muni_url)
-      muni_result = {}
+    muni.css('#id_KNvisningstabell tr').map { |row|
+      vals = row.css('td').map { |e| e.text.strip }
+      vals[0] = vals[0].gsub(/[.\/-]/, "").downcase;
 
-      muni.css('#id_KNvisningstabell tr').map { |row|
-        vals = row.css('td').map { |e| e.text.strip }
-        vals[0] = vals[0].gsub(/[.\/-]/, "").downcase;
-
-        unless vals[0].empty?
-          muni_result[vals[0]] = vals[1]
-        end
-      }
-
-      p muni_result
-
-      if muni_result["kommunenr"]
-        ScraperWiki.save_sqlite(["kommunenr"], muni_result)
+      unless vals[0].empty?
+        muni_result[vals[0]] = vals[1]
       end
+    }
+
+    pp muni_result
+
+    if muni_result["kommunenr"]
+      # ScraperWiki.save_sqlite(["kommunenr"], muni_result)
     end
   end
 end
